bacc47c55399a9fd23a771ac25baeecc0db0ca15
Rogan003 <veselin.roganovic.rogan003@gmail.com>
2026-02-02 15:56:00 +0100

    Initial Checkpoint 2 setup, with Grafana, InfluxDB and MQTT5 (forgotten changes 2)

--- simulators/door_button_one.py
+++ simulators/door_button_one.py
@@ -6,7 +6,7 @@ from colors import print_white
 
 def generate_value(callback, settings):
     if random.randint(0, 100) <= 15:
-        callback()
+        callback(settings)
         print_white("[Door 1] - Door opened")
 
 def run_door_button_one_simulator(delay, settings, callback, stop_event):
