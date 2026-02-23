f0ab0c0f7fef98f8661dc2293df69581e7c590d7
Rogan003 <veselin.roganovic.rogan003@gmail.com>
2026-02-23 04:13:55 +0100

    Refactored LED RGB a bit

--- rasp_pi_three/sensors/bedroom_rgb.py
+++ rasp_pi_three/sensors/bedroom_rgb.py
@@ -1,13 +1,12 @@
 import RPi.GPIO as GPIO
 import time
-from time import sleep
 from rgb_controller import rgb_controller
 
-RED_PIN = 12
-GREEN_PIN = 13
-BLUE_PIN = 19
-
 def run_bedroom_rgb_loop(settings, callback, stop_event):
+    RED_PIN = settings["red_pin"]
+    GREEN_PIN = settings["green_pin"]
+    BLUE_PIN = settings["blue_pin"]
+
     GPIO.setup(RED_PIN, GPIO.OUT)
     GPIO.setup(GREEN_PIN, GPIO.OUT)
     GPIO.setup(BLUE_PIN, GPIO.OUT)
@@ -56,73 +55,3 @@ def run_bedroom_rgb_loop(settings, callback, stop_event):
 
     while not stop_event.is_set():
         time.sleep(0.1)
-
-def turnOff():
-    GPIO.output(RED_PIN, GPIO.LOW)
-    GPIO.output(GREEN_PIN, GPIO.LOW)
-    GPIO.output(BLUE_PIN, GPIO.LOW)
-
-
-def white():
-    GPIO.output(RED_PIN, GPIO.HIGH)
-    GPIO.output(GREEN_PIN, GPIO.HIGH)
-    GPIO.output(BLUE_PIN, GPIO.HIGH)
-
-
-def red():
-    GPIO.output(RED_PIN, GPIO.HIGH)
-    GPIO.output(GREEN_PIN, GPIO.LOW)
-    GPIO.output(BLUE_PIN, GPIO.LOW)
-
-
-def green():
-    GPIO.output(RED_PIN, GPIO.LOW)
-    GPIO.output(GREEN_PIN, GPIO.HIGH)
-    GPIO.output(BLUE_PIN, GPIO.LOW)
-
-
-def blue():
-    GPIO.output(RED_PIN, GPIO.LOW)
-    GPIO.output(GREEN_PIN, GPIO.LOW)
-    GPIO.output(BLUE_PIN, GPIO.HIGH)
-
-
-def yellow():
-    GPIO.output(RED_PIN, GPIO.HIGH)
-    GPIO.output(GREEN_PIN, GPIO.HIGH)
-    GPIO.output(BLUE_PIN, GPIO.LOW)
-
-
-def purple():
-    GPIO.output(RED_PIN, GPIO.HIGH)
-    GPIO.output(GREEN_PIN, GPIO.LOW)
-    GPIO.output(BLUE_PIN, GPIO.HIGH)
-
-
-def lightBlue():
-    GPIO.output(RED_PIN, GPIO.LOW)
-    GPIO.output(GREEN_PIN, GPIO.HIGH)
-    GPIO.output(BLUE_PIN, GPIO.HIGH)
-
-
-if __name__ == "__main__":
-    try:
-        while True:
-            turnOff()
-            sleep(1)
-            white()
-            sleep(1)
-            red()
-            sleep(1)
-            green()
-            sleep(1)
-            blue()
-            sleep(1)
-            yellow()
-            sleep(1)
-            purple()
-            sleep(1)
-            lightBlue()
-            sleep(1)
-    except KeyboardInterrupt:
-        GPIO.cleanup()
