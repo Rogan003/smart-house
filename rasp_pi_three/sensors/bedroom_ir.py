59f5f75d4f742ed119d13218055ca46ed7db1df6
Rogan003 <veselin.roganovic.rogan003@gmail.com>
2026-02-23 04:56:02 +0100

    Tried something with bedroom IR

--- rasp_pi_three/sensors/bedroom_ir.py
+++ rasp_pi_three/sensors/bedroom_ir.py
@@ -11,23 +11,15 @@ from datetime import datetime
 import time
 
 # Static program vars
-pin = 17
 Buttons = [0x300ff22dd, 0x300ffc23d, 0x300ff629d, 0x300ffa857, 0x300ff9867, 0x300ffb04f, 0x300ff6897, 0x300ff02fd,
            0x300ff30cf, 0x300ff18e7, 0x300ff7a85, 0x300ff10ef, 0x300ff38c7, 0x300ff5aa5, 0x300ff42bd, 0x300ff4ab5,
            0x300ff52ad]  # HEX code list
 ButtonsNames = ["LEFT", "RIGHT", "UP", "DOWN", "2", "3", "1", "OK", "4", "5", "6", "7", "8", "9", "*", "0",
                 "#"]  # String list in same order as HEX list
 
-# Sets up GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(pin, GPIO.IN)
-
-
-# Gets binary value
-
-
 def run_bedroom_ir_loop(settings, callback, stop_event):
     pin = settings['pin']
+    GPIO.setmode(GPIO.BCM)
     GPIO.setup(pin, GPIO.IN)
 
     Buttons = [0x300ff22dd, 0x300ffc23d, 0x300ff629d, 0x300ffa857, 0x300ff9867, 0x300ffb04f, 0x300ff6897, 0x300ff02fd,
